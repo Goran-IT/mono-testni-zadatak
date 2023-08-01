aca4358cf1873c2ad660797c19dc543fa92c3cbd
Go-run <47899289+Goran-IT@users.noreply.github.com>
2023-08-01 12:05:07 +0200

    Update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ const Home = observer(()=> {
       </> }
 
       <main className="home">
-        <h1>Mono Testni Zadatak</h1>
+        <h1>Aplikacija za pretrazivanje automobilak</h1>
         <p>Ova aplikacija za prikazivanje automobila sadrži sisteme za:</p>
         <ul>
           <li>sortiranje</li>
